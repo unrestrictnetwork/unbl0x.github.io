54fbc44146fb60756750f7a8e2eb81d9b6809c5f
MikeLime <36614258+M1k3L1m3@users.noreply.github.com>
2019-09-20 21:50:34 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<html>
+	<head>
+		<title>My Drive - Google Drive</title>
+		<link rel="icon" href="https://ssl.gstatic.com/docs/doclist/images/infinite_arrow_favicon_4.ico">
+	</head>
+</html>
+
 <?php
 
 define('PROXY_START', microtime(true));
