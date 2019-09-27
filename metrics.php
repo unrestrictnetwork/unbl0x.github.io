f7d361e50169e0e6c6616944c6d27741201828d9
MikeLime <36614258+M1k3L1m3@users.noreply.github.com>
2019-09-27 11:01:13 -0500

    Update metrics.php

--- metrics.php
+++ metrics.php
@@ -1,9 +1,11 @@
-<?php
-$handle2 = fopen("views.txt", "r");
-$views = fgets($handle2);
-echo($views);
-fclose($handle2);
-$handle = fopen("views.txt", "w");
-fwrite(intval($views++;));
+<?php    
+$handle = fopen('views.txt', 'r'); // opens file for reading
+// fgets gets a line from the file as a string
+// intval converts a value to an integer
+$count = intval(fgets($handle));
+$count++;
+echo("$count");
 fclose($handle);
+$handle2 = fopen('views.txt', 'w');
+fwrite($handle2, "$count"); // writes count to the the file
 ?>
